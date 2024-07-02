5b35f783deb493a9f628a406e964ad80db278f31
MixDev <chaiwiwatmixx@gmail.com>
2024-07-02 11:24:59 +0700

    add customerRoute

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use("/customer")
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log("server start on port", port));
