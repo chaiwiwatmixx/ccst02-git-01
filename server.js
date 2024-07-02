17e0e66d971ff9b28efa218d2b59c7026e5ad919
MixDev <chaiwiwatmixx@gmail.com>
2024-07-02 10:33:36 +0700

    port config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(process.env.PORT || 8000, () => console.log("server start"))
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("server start on port", port));
